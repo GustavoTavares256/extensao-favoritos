4b53992a224f8ea444e17acc076ac4961f9b2ecf
GustavoTavares256 <263463812+GustavoTavares256@users.noreply.github.com>
2026-04-23 20:38:29 -0300

    .

--- extensao-codxis/popup.js
+++ extensao-codxis/popup.js
@@ -1,25 +1,24 @@
-document.addEventListener('DOMContentLoaded', function () {
-    
-    // Botão para abrir seu GitHub
-    const btnRepo = document.getElementById('btn-repo');
-    if (btnRepo) {
-        btnRepo.addEventListener('click', () => {
-            chrome.tabs.create({ url: 'https://github.com' });
-        });
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  // Botão para abrir seu GitHub
+  const btnRepo = document.getElementById("btn-repo");
+  if (btnRepo) {
+    btnRepo.addEventListener("click", () => {
+      chrome.tabs.create({ url: "https://github.com" });
+    });
+  }
 
-    // Botão para gerar UUID
-    const btnDados = document.getElementById('btn-gerar');
-    if (btnDados) {
-        btnDados.addEventListener('click', () => {
-            const uuid = crypto.randomUUID();
-            navigator.clipboard.writeText(uuid).then(() => {
-                const status = document.getElementById('status');
-                status.innerText = "Copiado com sucesso!";
-                setTimeout(() => {
-                    status.innerText = "";
-                }, 2000);
-            });
-        });
-    }
+  // Botão para gerar UUID
+  const btnDados = document.getElementById("btn-gerar");
+  if (btnDados) {
+    btnDados.addEventListener("click", () => {
+      const uuid = crypto.randomUUID();
+      navigator.clipboard.writeText(uuid).then(() => {
+        const status = document.getElementById("status");
+        status.innerText = "Copiado com sucesso!";
+        setTimeout(() => {
+          status.innerText = "";
+        }, 2000);
+      });
+    });
+  }
 });
